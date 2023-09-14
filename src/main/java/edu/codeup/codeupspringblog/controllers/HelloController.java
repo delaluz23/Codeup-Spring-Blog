da79f0feae0e4901663132f718d7d926f03fa76a
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-14 14:31:00 -0500

    first commit

---
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package edu.codeup.codeupspringblog.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello/world")
+    @ResponseBody
+    public String helloWorld(){
+        return "Hello WORLD";
+    }
+}
