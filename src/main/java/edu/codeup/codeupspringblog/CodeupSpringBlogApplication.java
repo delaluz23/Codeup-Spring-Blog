7868b6a96e8cf7bc5338d2e5e2a71dd5765276ca
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-18 10:17:08 -0500

    tymeleaf excercise

--- src/main/java/edu/codeup/codeupspringblog/CodeupSpringBlogApplication.java
+++ src/main/java/edu/codeup/codeupspringblog/CodeupSpringBlogApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodeupSpringBlogApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CodeupSpringBlogApplication.class, args);
     }
 
