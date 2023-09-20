56a213c49b95368bcaef051f10f3202d0978ef09
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-20 15:02:26 -0500

    services lecture

---
+++ src/main/java/edu/codeup/codeupspringblog/services/EmailService.java
@@ -0,0 +1,38 @@
+package edu.codeup.codeupspringblog.services;
+
+import edu.codeup.codeupspringblog.models.Ad;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class EmailService {
+
+    public JavaMailSender emailSender;
+
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public EmailService(JavaMailSender emailSender,) {
+        this.emailSender = emailSender;
+    }
+
+    public void prepareAndSend(Ad ad, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo("user@email.com");
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try {
+            this.emailSender.send(msg);
+        } catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+}
